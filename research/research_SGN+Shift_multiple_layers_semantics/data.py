e6dcc0746abd99c4b6216b2e76f6a7ac04f4e9c6
hao <hao.quan@icloud.com>
2020-07-21 12:41:27 +0200

    Start working 'research_SGN+Shift_multiple_layers_semantics_padding_300(WIP)'

--- research/research_SGN+Shift_multiple_layers_semantics/data.py
+++ research/research_SGN+Shift_multiple_layers_semantics/data.py
@@ -26,7 +26,7 @@ class CaloDataset(Dataset):
     def __init__(self, x, y):
         # All joints
         self.x = x
-        self.y = np.array(y[1])
+        self.y = np.array(y, dtype='int')
 
         # with open(self.label_path) as f:
         #     self.sample_name, self.label = pickle.load(f)
@@ -96,12 +96,12 @@ class CaloDataLoaders(object):
 
         # Numpy for semantic (Upper + Middle partion)
         self.train_X = np.load(path + "train_data_joint_upper_middle.npy")
-        self.test_X = np.load(path + "val_data_joint_upper_middle.npy")
+        self.test_X = np.load(path + "test_data_joint_upper_middle.npy")
 
         with open(path + 'train_label.pkl', 'rb') as f:
             self.train_Y = pkl.load(f)
 
-        with open(path + 'val_label.pkl', 'rb') as f:
+        with open(path + 'test_label.pkl', 'rb') as f:
             self.test_Y = pkl.load(f)
 
     def collate_fn_fix_train(self, batch):
---
+++ research/research_SGN+Shift_multiple_layers_semantics/data/calo/calo_data_divide_into_semantic_h5.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import os.path as osp
+import pickle as pkl
+
+key_train = 'train'
+key_test = 'test'
+path = osp.join('./')
+
+# H5 format
+train_X = pd.read_hdf(path + "data_training_test.h5",
+                           key=key_train + "_data").to_numpy()  # 35763x300x150
+train_Y = pd.read_hdf(path + "data_training_test.h5",
+                           key=key_train + "_label").to_numpy()  # 35763x300x150  # 35763
+
+test_X = pd.read_hdf(path + "data_training_test.h5",
+                          key=key_test + "_data").to_numpy()  # 35763x300x150
+test_Y = pd.read_hdf(path + "data_training_test.h5",
+                          key=key_test + "_label").to_numpy()  # 35763x300x150
+
+index_upper_middle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 22, 23, 28, 29, 30, 31, 32,
+                              33, 34, 35]
+train_x_upper_middle = np.stack([train_X[:, i] for i in index_upper_middle]).transpose(1, 0)
+
+test_x_upper_middle = np.stack([test_X[:, i] for i in index_upper_middle]).transpose(1, 0)
+
+with open('train_data_joint_upper_middle.npy', 'wb') as f:
+    np.save(f, train_x_upper_middle)
+
+with open('train_label.pkl', 'wb') as f:
+    pkl.dump(train_Y, f)
+
+with open('test_data_joint_upper_middle.npy', 'wb') as f:
+    np.save(f, test_x_upper_middle)
+
+with open('test_label.pkl', 'wb') as f:
+    pkl.dump(test_Y, f)
+
+
+print("")
